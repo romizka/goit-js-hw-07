9aced342c19c8265c7700a10d105e12bbd1a0e14
romizka <awencka1@gmail.com>
2023-03-30 21:12:10 +0200

    doing homework (30.03.23)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.innerHTML = galleryItems.map(item => `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}" />
+    </a>
+  </div>
+`).join('');
+
+function handleGalleryItemClick(event) {
+    event.preventDefault();
+  
+    const imageSrc = event.target.dataset.source;
+    const instance = basicLightbox.create(`<img src="${imageSrc}" width="800" height="600">`);
+    instance.show();
+  }
+
+const galleryImages = document.querySelectorAll('.gallery__image');
+galleryImages.forEach(image => {
+  image.addEventListener('click', handleGalleryItemClick);
+});
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.innerHTML = galleryItems.map(item => `
+<li class="gallery__item">
+<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>
+</li>
+`).join('');
+
+
+  galleryContainer.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captions: true,
+        captionPosition: 'bottom',
+        captionType: 'attr',
+        captionsData: 'alt',
+        captionDelay: 250,
+      });
+  })
