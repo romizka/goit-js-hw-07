8c13eadd5514bab379548d3214ab0bb7fa7b226e
romizka <awencka1@gmail.com>
2023-04-02 11:53:56 +0200

    changing of addEventListener target to individual photos

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,24 +5,27 @@ console.log(galleryItems);
 
 
 const galleryContainer = document.querySelector('.gallery');
+const galleryLinks = document.querySelectorAll('.gallery__link');
 
 galleryContainer.innerHTML = galleryItems.map(item => `
 <li class="gallery__item">
-<a class="gallery__item" href="${item.original}">
+<a class="gallery__link" href="${item.original}">
   <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
 </a>
 </li>
 `).join('');
 
 
-  galleryContainer.addEventListener('click', (e) => {
+galleryLinks.forEach(link => {
+  link.addEventListener('click', (e) => {
     e.preventDefault();
 
-    const lightbox = new SimpleLightbox('.gallery a', {
+    const lightbox = new SimpleLightbox(link, {
         captions: true,
         captionPosition: 'bottom',
         captionType: 'attr',
         captionsData: 'alt',
         captionDelay: 250,
       });
-  })
+  });
+});
